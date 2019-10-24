0996e92fc326dc4bbbeaf0704f5ffabb2409867e
Kacey Gambill <kacey.gam@gmail.com>
2019-10-24 12:50:38 -0600

    added week-view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeekViewComponent } from './week-view/week-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeekViewComponent
   ],
   imports: [
     BrowserModule,
