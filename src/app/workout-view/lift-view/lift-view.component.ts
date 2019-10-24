ba5e9e82dabe9cb1e7bd25389bf2357a5b58cb21
Kacey Gambill <kacey.gam@gmail.com>
2019-10-24 15:16:13 -0600

    added majority of components

---
+++ src/app/workout-view/lift-view/lift-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-lift-view',
+    templateUrl: './lift-view.component.html',
+    styleUrls: ['./lift-view.component.css']
+})
+export class LiftViewComponent {
+    
+}
